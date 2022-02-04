6cefc99ad65fa62669d17d4b8ff7d7cc516c97b9
JaTGin <jakeginiewski@gmail.com>
2022-02-04 13:43:28 -0500

    fixed repo

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # sse-mod-project
+
 Skyrim: Special Edition mod for IGME 590
 Check the <a href="https://docs.google.com/document/d/1I-2l-IQQxsAhprYQ2-kzCOAbcsb03X-nemtjJsi-2Ro/edit#">Documentation</a> for tips on this project's technical side
+INSTALLATION
+
+1. Clone repo to skyrim's data folder
+2. Run install.ps1
+3. Hit 'y' when asked to install the mod to your Skyrim folder
+4. Edit the mod in SCK
+   PUSHING
+5. Run install.ps1
+6. Hit 'n' when asked to install
+7. hit 'y' when asked to pull data from the Skyrim folder
+8. Commit and push
--- install.ps1
+++ install.ps1
@@ -1,50 +1,14 @@
-function Write-Path {
-    $path | Out-File -FilePath path.txt
-}
-
-function Read-Path {
-    Write-Host "here"
-    if (Get-Content .\path.txt) {
-        Write-Host "if passed"
-        $path = Get-Content .\path.txt
-        Write-Host $path
-    }
-}
-
 function Install-Mod {
-    Copy-Item -Path .\installation\* -Destination $path -PassThru
+    Copy-Item -Path .\installation\* -Destination ../ -PassThru
 }
 
 function Update-Mod {
-    Copy-Item -Path $path\the-sun-may-rise-again.esp -Destination .\installation\* -PassThru
-    Copy-Item -Path $path\scripts\* -Destination .\installation\* -PassThru
+    Copy-Item -Path ../the-sun-may-rise-again.esp -Destination .\installation\ -PassThru
+    Copy-Item -Path ../111test.txt -Destination .\installation\ -PassThru
 }
 
 function Install-Scripts {
     #TBD when we start scripting
-}
-
-Read-Path
-if ($path) {
-    Install-Mod
-}
-else {
-    Write-Host "SEARCHING FOR DIRECTORY... "
-    if (gci -Recurse -Filter "Skyrim Special Edition" -Directory -ErrorAction SilentlyContinue -Path "C:\Program Files (x86)\Steam\steamapps\common") {
-        $path = "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition\Data"
-        Write-Host "FOUND PATH AT " $path
-        Write-Path
-    }
-    else {
-        if (gci -Recurse -Filter "Skyrim Special Edition" -Directory -ErrorAction SilentlyContinue -Path "D:\SteamLibrary\steamapps\common") {
-            $path = "D:\SteamLibrary\steamapps\common\Skyrim Special Edition\Data"
-            Write-Host "FOUND PATH AT " $path
-            Write-Path
-        }
-        else {
-            Write-Host "PATH SEARCH FAILED- TALK TO JAKE ABOUT IT"
-        }
-    }
 }
 
 $installyn = Read-Host "Install mod to Skyrim folder? Y/N"
