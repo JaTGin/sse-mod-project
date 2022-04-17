dd5d8d06974a7ce1413b353de66e64e70d44b765
JaTGin <jakeginiewski@gmail.com>
2022-04-17 15:03:16 -0400

    Created process for PSC extraction

--- install.ps1
+++ install.ps1
@@ -1,24 +1,8 @@
-function Install-Mod {
-    Copy-Item -Path .\installation\* -Destination ../ -PassThru
-}
-
-function Update-Mod {
-    Copy-Item -Path ../the-sun-may-rise-again.esp -Destination .\installation\ -PassThru
-}
-
-function Install-Scripts {
-    #TBD when we start scripting
-}
-
-$installyn = Read-Host "Install mod to Skyrim folder? Y/N"
-if ("Y" -eq $installyn) {
-    Write-Host "Installing... "
-    Install-Mod
-}
-else {
-    $installyn = Read-Host "Pull data from Skyrim folder? Y/N"
-    if ("Y" -eq $installyn) {
-        Write-Host "Updating Repo... "
-        Update-Mod
-    }
-}
+Write-Host "Extracting PSCs..."
+Copy-Item -Path ../Source/Scripts/TIF__02011AFF.psc -Destination .\installation\SrcScripts\ -PassThru
+Copy-Item -Path ../Source/Scripts/TIF__02011B01.psc -Destination .\installation\SrcScripts\ -PassThru
+Copy-Item -Path ../Source/Scripts/TIF__02011B06.psc -Destination .\installation\SrcScripts\ -PassThru
+Copy-Item -Path ../Source/Scripts/TIF__0201207C.psc -Destination .\installation\SrcScripts\ -PassThru
+Copy-Item -Path ../Source/Scripts/TIF__02012082.psc -Destination .\installation\SrcScripts\ -PassThru
+Copy-Item -Path ../Source/Scripts/TIF__02012092.psc -Destination .\installation\SrcScripts\ -PassThru
+Write-Host "PSCs Extracted."
